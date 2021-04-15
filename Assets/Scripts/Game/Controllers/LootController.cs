5e854daaa015efb7340db20989824e8af5ac2c72
LiamCMoore <liammoore96@gmail.com>
2021-04-15 21:32:15 +1000

    Drop System Implemented
    Ships may now drop module components. When Asteroids are implemented, they will be able to drop Rare metals.

--- Assets/Scripts/Game/Controllers/LootController.cs
+++ Assets/Scripts/Game/Controllers/LootController.cs
@@ -1,16 +1,55 @@
-using Celeritas.Scriptables;
+using Celeritas.Extensions;
+using Celeritas.Game;
+using Celeritas.Game.Controllers;
+using Celeritas.Game.Entities;
 using Sirenix.OdinInspector;
-using System.Collections.Generic;
-using System.Collections;
 using UnityEngine;
 
 namespace Celeritas.Game.Entities
 {
 	public class LootController : Singleton<LootController>
 	{
-		public void LootDrop(float dropValue, bool isBoss, string dropType)
+		/// <summary>
+		/// The attatched ship data.
+		/// </summary>
+		public int ModuleComponents { get; private set; }
+
+		/// <summary>
+		/// The quanitity of Rare Metals the player has. This should be set to the players out-of-run rare metals at the start of the run scene.
+		/// </summary>
+		public int RareMetals { get; private set; }
+
+		public void LootDrop(float dropValue, bool isBoss, string dropType, Vector3 dropLocation)
 		{
-			Debug.Log(dropValue + " and " + isBoss + " and " + dropType);
+			if (dropType == "EnemyShip")
+            {
+				float lootRoll = Random.Range(100, 10100);
+				lootRoll = (lootRoll - 100);
+				lootRoll = (lootRoll / 100);
+
+				if (dropValue > lootRoll)
+                {
+					ModuleComponents = ModuleComponents + 1;
+					Debug.Log("Module dropped! You now have: " + ModuleComponents);
+
+					//var drop = EntityDataManager.InstantiateEntity<*LOOT DROP VISUAL*>;
+				}
+			}
+
+			if (dropType == "Asteroid")
+			{
+				float lootRoll = Random.Range(100, 10100);
+				lootRoll = (lootRoll - 100);
+				lootRoll = (lootRoll / 100);
+
+				if (dropValue > lootRoll)
+				{
+					RareMetals = RareMetals + Random.Range(1, 10);
+					Debug.Log("Rare metals dropped! You now have: " + RareMetals);
+
+					//var drop = EntityDataManager.InstantiateEntity<*LOOT DROP VISUAL*>;
+				}
+			}
 		}
 	}
 }
