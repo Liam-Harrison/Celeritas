baa7e1ae136ddc036a8062f6f6cc5b413cde2fa6
Liam Harrison <liam.har98@gmail.com>
2021-04-22 01:02:21 +1000

    removed unused enum

--- Assets/Scripts/Game/Entity.cs
+++ Assets/Scripts/Game/Entity.cs
@@ -8,13 +8,6 @@ using UnityEngine;
 
 namespace Celeritas.Game
 {
-	public enum EntityType
-	{
-		Projectile,
-		Ship,
-
-	}
-
 	/// <summary>
 	/// The Entity class provides basic functionality for on-screen objects.
 	/// </summary>
