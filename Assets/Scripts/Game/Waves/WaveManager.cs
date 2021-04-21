0f47a5a6c227273898205694e28311825ca8f6da
TeaRussell <t.russell.au@gmail.com>
2021-04-22 01:20:32 +1000

    + basic randomised waves

--- Assets/Scripts/Game/Waves/WaveManager.cs
+++ Assets/Scripts/Game/Waves/WaveManager.cs
@@ -11,12 +11,51 @@ namespace Celeritas.Game
 {
 	public class WaveManager : Singleton<WaveManager>
 	{
+		private List<int> WaveLootValues;
+		private uint nextWave;
+
+		protected override void Awake()
+		{
+			nextWave = 0;
+			//TODO: read WaveLootValues from file.
+			WaveLootValues = new List<int>() { 5, 7, 10 };
+		}
+
 		[SerializeField]
 		public WaveData[] data;
+		private int waveAll = 0;
+		[SerializeField]
+		public int MinShips = 5;
 
 		public bool WaveActive { get; private set; }
 
-		private uint nextWave;
+		/// <summary>
+		/// Start a wave.
+		/// </summary>
+		public void StartWave()
+		{
+			Debug.Log("StartWave");
+			List<ShipData> NewWave = new List<ShipData>();
+			for (int i = 0; i < 30; i++)
+			{
+				Debug.Log("Generate Wave: #" + i);
+				GenerateWave(NewWave);
+				Debug.Log("Ships: " + NewWave.Count);
+				if (NewWave.Count >= MinShips)
+				{
+					break;
+				}
+			}
+
+			//If adding wave shapes, modify here
+			foreach (ShipData ship in NewWave)
+			{
+				EnemyManager.Instance.SpawnShip<Celeritas.AI.AIBasicChase>(ship, PlayerController.Instance.ShipEntity.gameObject.transform.position.RandomPointOnCircle(10f));
+			}
+			nextWave++;
+		}
+
+		System.Random random = new System.Random();
 
 		public static event Action OnWaveEnded;
 		public static event Action OnWaveStarted;
@@ -25,9 +64,23 @@ namespace Celeritas.Game
 
 		public IReadOnlyDictionary<WaveData, List<ShipEntity>> Waves { get => ships; }
 
-		protected override void Awake()
+		private void GenerateWave(List<ShipData> Wave)
 		{
-			nextWave = 0;
+			float waveLootTotal = WaveLootValues[0];
+			int waveType = waveAll; //FIX WHEN LOGIC KNOWN omfg
+			int randShip;
+			if (data[waveType].IsBoss)
+			{
+				Wave.Add(data[waveType].BossShip);
+				waveLootTotal -= data[waveType].BossShip.Prefab.GetComponent<ShipEntity>().LootConfig.Gain;
+			}
+			while (waveLootTotal > 0)
+			{
+				randShip = random.Next(0, data[waveType].ShipPool.Length);
+				Wave.Add(data[waveType].ShipPool[randShip]);
+				waveLootTotal -= data[waveType].ShipPool[randShip].Prefab.GetComponent<ShipEntity>().LootConfig.Gain;
+			}
+			Debug.Log("Loot Total: " + waveLootTotal);
 		}
 
 		public void StartWave()
