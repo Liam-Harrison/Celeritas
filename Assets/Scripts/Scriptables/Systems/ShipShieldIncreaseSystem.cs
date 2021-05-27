091131282ccdecdf1f57261c9756b8c688fb704e
LiamCMoore <42259108+LiamCMoore@users.noreply.github.com>
2021-05-28 03:03:25 +1000

    Shield Boost Module (#149)
    * Module Work in Progress (UNSTABLE)
    
    Working on the shields increase module, unstable branch.
    
    * Shield Increase Module
    
    Finished the shield increase module.
    
    * Prefab changes and ship updates
    
    * Module fixed and functional, module sizes standardized
    
    Module sizes have been standardized to match the size described in the documentation
    
    * Floor groups changed to match module sizes
    
    Floor groups updated

---
+++ Assets/Scripts/Scriptables/Systems/ShipShieldIncreaseSystem.cs
@@ -0,0 +1,59 @@
+using Celeritas.Game;
+using Celeritas.Game.Entities;
+using Celeritas.Scriptables.Interfaces;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using System;
+
+namespace Celeritas.Scriptables.Systems
+{
+	[CreateAssetMenu(fileName = "New Shield Increase Modifier", menuName = "Celeritas/Modifiers/Shield Increase")]
+	public class ShipShieldIncreaseSystem : ModifierSystem, IEntityEffectAdded, IEntityEffectRemoved
+	{
+
+		[SerializeField, PropertyRange(0, 5), InfoBox("Percentage to add, 1 (default shield amount + 1 (value) = 2 (200% shield amount)")]
+		private float amount;
+
+		[SerializeField, PropertyRange(0, 2), InfoBox("Percentage extra to add per level")]
+		private float amountExtraPerLevel;
+
+		/// <summary>
+		/// The amount this system modifies shield quantity
+		/// </summary>
+		public float Amount { get => amount; }
+
+		/// <summary>
+		/// How much extra the shields are increased per level
+		/// </summary>
+		public float AmountExtraPerLevel { get => amountExtraPerLevel; }
+
+		/// <inheritdoc/>
+		public override bool Stacks => true;
+
+		/// <inheritdoc/>
+		public override SystemTargets Targets => SystemTargets.Ship;
+
+		public void OnEntityEffectAdded(Entity entity, ushort level)
+		{
+			// TODO: may need to update effect when system levels up, depending on how game loop works.
+			// otherwise effects may not reflect levels
+
+			var ship = entity as ShipEntity;
+			float amountToAdd = 1 + amount + (level * amountExtraPerLevel);
+
+			uint newValue = (uint)(ship.Shield.MaxValue * amountToAdd);
+			ship.Shield.MaxValue = newValue;
+
+		}
+
+		public void OnEntityEffectRemoved(Entity entity, ushort level)
+		{
+			var ship = entity as ShipEntity;
+			float amountToAdd = 1 + amount + (level * amountExtraPerLevel);
+
+			uint newValue = (uint)(ship.Shield.MaxValue / amountToAdd);
+
+			ship.Shield.MaxValue = newValue;
+		}
+	}
+}
