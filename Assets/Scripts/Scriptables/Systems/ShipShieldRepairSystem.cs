ddb6db33e35f8ae1ecf55bb94feed50baa32fab2
Anthony Tran <100575155@student.swin.edu.au>
2021-08-07 18:21:36 +1000

    Added Ship Repair Module
    Added Ship Repair Module

---
+++ Assets/Scripts/Scriptables/Systems/ShipShieldRepairSystem.cs
@@ -0,0 +1,88 @@
+﻿using Celeritas.Game;
+using Celeritas.Game.Entities;
+using Celeritas.Scriptables.Interfaces;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Celeritas.Scriptables.Systems
+{
+
+	[CreateAssetMenu(fileName = "New Ship Shield Repair Modifier", menuName = "Celeritas/Modifiers/Ship Shield Repair")]
+
+	public class ShipShieldRepairSystem : ModifierSystem, IEntityEffectAdded, IEntityEffectRemoved, IEntityUpdated
+	{
+
+		[SerializeField, PropertyRange(0, 3), Title("Percentage repaired per second")]
+		private float amount;
+
+		[SerializeField, PropertyRange(0, 5), Title("Percentage added per level")]
+		private float amountExtraPerLevel;
+
+		/// <summary>
+		/// The amount this system modifies shield repair
+		/// </summary>
+		public float Amount { get => amount; }
+
+		/// <summary>
+		/// How much extra shields are repaired per level
+		/// </summary>
+		public float AmountExtraPerLevel { get => amountExtraPerLevel; }
+
+		public override bool Stacks => true;
+
+		public override SystemTargets Targets => SystemTargets.Ship;
+
+		public override string GetTooltip(ushort level) => $"<color=green>▲</color> Increase shield repair rate by <color=green>{(Amount + (AmountExtraPerLevel * level)) * 100:0}%</color>.";
+
+		/// <summary>
+		/// Determines whether the shield repairs or not.
+		/// </summary>
+		private bool IsActive = false;
+
+		public void OnEntityEffectAdded(Entity entity, ushort level)
+		{
+			var ship = entity as ShipEntity;
+			IsActive = true;
+			nextTime = (Time.time + interval);
+		}
+
+		public void OnEntityEffectRemoved(Entity entity, ushort level)
+		{
+			var ship = entity as ShipEntity;
+			IsActive = false;
+		}
+
+		/// <summary>
+		/// Seconds between repairs
+		/// </summary>
+		private int interval = 1;
+
+		/// <summary>
+		/// Timer
+		/// </summary>
+		private float nextTime = 0;
+
+		public void OnEntityUpdated(Entity entity, ushort level)
+		{
+			var ship = entity as ShipEntity;
+
+			
+			float amountToAdd = (ship.Shield.MaxValue / 100) * (amount + (level * amountExtraPerLevel));
+
+			if (IsActive == true)
+			{
+				if (Time.time > nextTime)
+				{
+					if (ship.Shield.CurrentValue < ship.Shield.MaxValue)
+					{
+						
+						ship.Shield.Damage(Mathf.RoundToInt(-1 * amountToAdd));
+
+					}
+					nextTime = (nextTime + interval);
+		
+				}
+			}
+		}
+	}
+}
