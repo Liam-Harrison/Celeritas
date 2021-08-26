7ef7ea0d97816f16f379d224a88c9bd0f688da32
MistaMishMash <MistaMishMash@users.noreply.github.com>
2021-08-26 16:12:21 +1000

    removed leftover comments

--- Assets/Scripts/Scriptables/Systems/CreateSubprojectilesSystem.cs
+++ Assets/Scripts/Scriptables/Systems/CreateSubprojectilesSystem.cs
@@ -69,7 +69,6 @@ namespace Assets.Scripts.Scriptables.Systems
 
 		private void SpawnProjectiles(Entity entity)
 		{
-			Debug.Log("ping");
 			ProjectileEntity projectile = entity as ProjectileEntity;
 
 			for (int i = 0; i < numberToSpawn; i++)
@@ -100,9 +99,6 @@ namespace Assets.Scripts.Scriptables.Systems
 				else
 					proj = EntityDataManager.InstantiateEntity<ProjectileEntity>(shrapnel, entity.Position, q, projectile.Weapon, effects);
 				proj.ParentProjectile = projectile;
-				Debug.Log(proj.transform.position);
-				Debug.Log(entity.GetInstanceID());
-				Debug.Log(entity.transform.position);
 
 			}
 		}
