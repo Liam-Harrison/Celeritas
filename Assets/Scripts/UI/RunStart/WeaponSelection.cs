2e839549634a4f81b7ad73ce22a761abef0d828a
MistaMishMash <MistaMishMash@users.noreply.github.com>
2021-09-03 14:35:38 +1000

    Player Startup Weapon Equipment (#256)
    * Logic is in place and working.
    
    * replaced autocannons in all player ships with placeholder weapon
    
    (placeholder weapon is the same as autocannon just with a placeholder field == positive)
    
    * Moved most logic into a script in the Selection panel, to make things a bit neater.
    
    * Updated empty weapon data title to be more intuitive for future devs, and error text anchor.
    
    * Removed redundant comment

--- Assets/Scripts/UI/RunStart/WeaponSelection.cs
+++ Assets/Scripts/UI/RunStart/WeaponSelection.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 namespace Celeritas.UI.Runstart
 {
@@ -31,6 +32,9 @@ namespace Celeritas.UI.Runstart
 
 		[SerializeField, TitleGroup("Dragging")]
 		private IconUI dragIcon;
+
+		[SerializeField, TitleGroup("ErrorText")]
+		private GameObject errorText; // used to show 'please select a weapon before launching' text
 
 		private readonly List<WeaponPanel> panels = new List<WeaponPanel>();
 
@@ -73,6 +77,7 @@ namespace Celeritas.UI.Runstart
 				var parent = view.x > 0.5f ? rightGrid : leftGrid;
 
 				var panel = Instantiate(panelPrefab, parent).GetComponent<WeaponPanel>();
+					
 				panel.SetModule(weapon.AttatchedModule);
 				panel.SetWeapon(weapon.WeaponData);
 
@@ -126,6 +131,8 @@ namespace Celeritas.UI.Runstart
 
 			foreach (var weapon in EntityDataManager.Instance.Weapons)
 			{
+				if (weapon.Placeholder) // don't let players equip placeholder weapons
+					continue;
 				var panel = Instantiate(itemPrefab, itemContent).GetComponent<WeaponItem>();
 				panel.SetWeapon(weapon);
 			}
@@ -155,6 +162,22 @@ namespace Celeritas.UI.Runstart
 			dragIcon.gameObject.SetActive(false);
 			Dragging = false;
 			DraggingItem = null;
+
+			// if no placeholder weapons are left, ready to launch
+			bool readyToLaunch = true;
+			foreach (WeaponPanel w in panels)
+			{
+				if (w.Weapon.Placeholder)
+				{ 
+					readyToLaunch = false;
+					break;
+				}
+			}
+			if (readyToLaunch)
+			{
+				// remove error message if all weapons are equipped correctly.
+				errorText.SetActive(false); 
+			}
 		}
 
 		private bool TryGetPanel(out WeaponPanel panel)
