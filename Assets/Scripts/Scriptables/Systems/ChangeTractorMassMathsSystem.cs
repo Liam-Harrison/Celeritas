43affbd0351e60ebe80dc227293060fbb8cf0fcc
MistaMishMash <MistaMishMash@users.noreply.github.com>
2021-09-21 18:06:28 +1000

    Logic for tractor ignoring mass is in. Just need to make module data.

---
+++ Assets/Scripts/Scriptables/Systems/ChangeTractorMassMathsSystem.cs
@@ -0,0 +1,58 @@
+﻿using Assets.Scripts.Game.Controllers;
+using Celeritas.Game;
+using Celeritas.Scriptables;
+using Celeritas.Scriptables.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Scriptables.Systems
+{
+	/// <summary>
+	/// Changes how 'heavy' the tractor beam considers whatever it is holding.
+	/// Note that all values are multipliers. 1 = 100%
+	/// </summary>
+	class ChangeTractorMassMathsSystem: ModifierSystem, IEntityEffectAdded, IEntityEffectRemoved
+	{
+
+		[SerializeField]
+		private float startingMultiplier;
+
+		[SerializeField]
+		private float amountToAddPerLevel;
+		// also a multiplier. so 0.2 == add 20% each level
+		// so if starting multiplier = 0.5, and toAdd is 0.1, level 1 = 0.5, level 2 = 0.6, ect.
+
+		public override bool Stacks => false;
+
+		public override SystemTargets Targets => SystemTargets.Ship;
+
+		public override string GetTooltip(ushort level) => $"Reduces the mass of tractor-beamed objects by {startingMultiplier + (amountToAddPerLevel * level) * 100}%";
+
+		public void OnEntityEffectAdded(Entity entity, ushort level)
+		{
+			// check if entity is player
+			if (entity.PlayerShip)
+			{
+				TractorBeamController.Instance.TargetMassMultiplier += factorToAdd();
+			}
+		}
+
+		public void OnEntityEffectRemoved(Entity entity, ushort level)
+		{
+			if (entity.PlayerShip)
+			{
+				TractorBeamController.Instance.TargetMassMultiplier -= factorToAdd();
+			}
+		}
+
+		private float factorToAdd()
+		{
+			return startingMultiplier + (amountToAddPerLevel * amountToAddPerLevel);
+		}
+
+	}
+}
