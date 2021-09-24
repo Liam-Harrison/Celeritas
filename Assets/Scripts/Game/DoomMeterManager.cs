83ee85ec32cf1af63696217f188cde241c292490
Anthony Tran <100575155@student.swin.edu.au>
2021-09-24 13:26:44 +1000

    Saved UI elements as a prefab.
    Saved UI elements as a prefab.
    Disabled doom timer in scene since slider looks simpler.

--- Assets/Scripts/Game/DoomMeterManager.cs
+++ Assets/Scripts/Game/DoomMeterManager.cs
@@ -57,8 +57,8 @@ namespace Celeritas.Game
 				if (timeLeft > 0)
 				{
 					timeLeft -= Time.deltaTime;
-					ShowTime(timeLeft);
 					timeElapsed = Mathf.FloorToInt(timer - timeLeft);
+					ShowTime(timeLeft);
 				}
 				else
 				{
