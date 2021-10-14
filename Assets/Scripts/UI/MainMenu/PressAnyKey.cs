fa6915cf22c02177b91792bef277aa05b0d98e96
Anthony Tran <100575155@student.swin.edu.au>
2021-10-14 17:23:04 +1100

    Updated PressAnyKey.cs
    Updated PressAnyKey.cs
    
    Noticed you could activate the script without activating the animation.
    Technically this shouldn't be possible but unity sucks.

--- Assets/Scripts/UI/MainMenu/PressAnyKey.cs
+++ Assets/Scripts/UI/MainMenu/PressAnyKey.cs
@@ -7,25 +7,24 @@ using UnityEngine;
 public class PressAnyKey : MonoBehaviour
 {
 	public Button button;
-
-	private bool buttonPressed = false;
+	public CanvasGroup canvas;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+
+	}
 
     // Update is called once per frame
     void Update()
     {
-		if (buttonPressed == false)
+		if (Keyboard.current.anyKey.wasPressedThisFrame)
 		{
-			if (Keyboard.current.anyKey.wasPressedThisFrame)
-			{
-				button.onClick.Invoke();
-				buttonPressed = true;
-			}
+			button.onClick.Invoke();
 		}
-    }
+		if (canvas.alpha > 0)
+		{
+			this.enabled = false;
+		}
+	}
 }
