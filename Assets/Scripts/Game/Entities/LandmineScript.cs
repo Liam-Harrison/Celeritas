57c6f9de6f4636777f0cea566ccec8b1fec8ab88
Anthony Tran <100575155@student.swin.edu.au>
2021-10-24 17:17:50 +1100

    Updated LandmineScript
    Updated LandmineScript to include damage notification

--- Assets/Scripts/Game/Entities/LandmineScript.cs
+++ Assets/Scripts/Game/Entities/LandmineScript.cs
@@ -1,8 +1,9 @@
+using Assets.Scripts.Scriptables.Data;
 using Celeritas.Game.Controllers;
 using Celeritas.Scriptables;
+using Celeritas.UI;
 using Sirenix.OdinInspector;
 using System.Collections.Generic;
-using System.Collections;
 using UnityEngine;
 
 namespace Celeritas.Game.Entities
@@ -11,33 +12,38 @@ namespace Celeritas.Game.Entities
 	/// 
 	/// </summary>
 
-	public class LandmineScript : MonoBehaviour
+	public class LandmineScript : Entity
 	{
+		public override SystemTargets TargetType { get => SystemTargets.Projectile; }
 
 		private float damage = 150;
 
-		private float duration = 1;
-
 		private Material material;
+
+		private EntityStatBar health;
+
+		public EntityStatBar Health { get => health; }
+
+		public Rigidbody2D Rigidbody { get; private set; }
 
 		// Start is called before the first frame update
 		void Start()
 		{
 			material = GetComponent<MeshRenderer>().material;
-			
+			material.color = Color.red;
 		}
 
 		// Update is called once per frame
-		void Update()
-		{
-			material.color = Color.Lerp(Color.red, Color.blue, Mathf.PingPong(Time.time, 1));
-		}
+		//void Update()
+		//{
+		//	material.color = Color.Lerp(Color.red, Color.blue, Mathf.PingPong(Time.time, 1));
+		//}
 
-		public void initialize(float setDamage, float setduration)
+		public void initialize(float setDamage, float duration)
 		{
 			damage = setDamage;
-			duration = setduration;
-			StartCoroutine(DestroyOverTime());
+			health = new EntityStatBar(1.0f);
+			Object.Destroy(gameObject, duration);
 		}
 
 		private void OnTriggerEnter2D(Collider2D other)
@@ -45,11 +51,20 @@ namespace Celeritas.Game.Entities
 			var entity = other.gameObject.GetComponentInParent<Entity>();
 			if (entity != null)
 			{
+				if (entity.PlayerShip == true)
+				{
+					return;
+				}
+				if (entity is ProjectileEntity)
+				{
+					var projectile = entity as ProjectileEntity;
+					CombatHUD.Instance.PrintFloatingText(this, projectile.Damage);
+				}
 				OnEntityHit(entity);
 			}
 		}
 
-		public void OnEntityHit(Entity other)
+		public override void OnEntityHit(Entity other)
 		{
 			if (other.PlayerShip == true)
 			{
@@ -60,12 +75,6 @@ namespace Celeritas.Game.Entities
 				other.TakeDamage(other, damage);
 				Destroy(gameObject);
 			}
-		}
-
-		IEnumerator DestroyOverTime()
-		{
-			yield return new WaitForSeconds(duration);
-			Destroy(gameObject);
 		}
 	}
 }
